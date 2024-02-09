5f7fb661476c2647f178d064ddb7a17b61bfc03b
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-10 04:00:16 +0600

    exp section aos animation changed

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -153,7 +153,7 @@ const Experience = () => {
         </div>
 
         <div className="bg-slate-950 text-left py-2 my-4 rounded">
-          <p data-aos="fade-left" className="pl-10">
+          <p data-aos="fade-up" className="pl-10">
             MongoDB
           </p>
           <progress
